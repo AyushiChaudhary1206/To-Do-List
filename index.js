9565da5f26ead8e90cc42e9cc21967b9afe1ec23
AyushiChaudhary1206 <154445679+AyushiChaudhary1206@users.noreply.github.com>
2023-12-22 11:28:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("input")
+const list = document.getElementById("listcontainer")
+const button = document.querySelector(".btn")
+button.addEventListener("click", () => {
+    if (input.value === '') {
+        alert("you must write something")
+    }
+    else {
+        let li = document.createElement("li")
+        li.innerHTML = input.value
+        listcontainer.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    input.value = '';
+    saveData();})
+listcontainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+function saveData() {
+    localStorage.setItem("data", listcontainer.innerHTML)
+}
+function showList() {
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showList();
